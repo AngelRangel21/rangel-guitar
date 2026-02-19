759be12963a02e7ab2151b2e5987e93408f5c4da
Angel Rangel <angel145256@gmail.com>
2026-02-19 13:42:31 -0600

    refactor: unificar el estilo de comillas y mejorar la legibilidad en el componente HomeClient

--- src/components/home-client.tsx
+++ src/components/home-client.tsx
@@ -1,11 +1,12 @@
-"use client";
+'use client'
 
-import { Header } from "@/components/header/Header";
-import { Footer } from "@/components/footer";
-import { SongList } from "@/components/song-list";
-import type { Song } from "@/lib/types";
-import { useI18n } from "@/context/i18n-context";
-import { SongSearch } from "./search";
+import { Header } from '@/components/header/Header'
+import { Footer } from '@/components/footer'
+import { SongList } from '@/components/song-list'
+import type { Song } from '@/types'
+import { useI18n } from '@/context/i18n-context'
+import { SongSearch } from './search'
+import { JSX } from 'react'
 
 /**
  * Componente de cliente para la página de inicio.
@@ -13,18 +14,18 @@ import { SongSearch } from "./search";
  * @param {{ initialSongs: Song[] }} props - Propiedades que contienen la lista inicial de canciones.
  * @returns {JSX.Element} El componente de la página de inicio.
  */
-export function HomeClient({ initialSongs }: { initialSongs: Song[] }) {
+export function HomeClient ({ initialSongs }: { initialSongs: Song[] }): JSX.Element {
   const { t } = useI18n()
 
   return (
     <>
       {/* El encabezado recibe el término de búsqueda y la función para actualizarlo. */}
       <Header />
-      <main className="grow container mx-auto px-4 py-8 space-y-6">
+      <main className='grow container mx-auto px-4 py-8 space-y-6'>
         <SongSearch />
-        <div className="flex justify-between items-center">
-          <h2 className="text-3xl font-bold text-foreground">
-            {t("allSongs")}
+        <div className='flex justify-between items-center'>
+          <h2 className='text-3xl font-bold text-foreground'>
+            {t('allSongs')}
           </h2>
         </div>
         {/* La lista de canciones recibe las canciones filtradas para renderizar. */}
@@ -32,5 +33,5 @@ export function HomeClient({ initialSongs }: { initialSongs: Song[] }) {
       </main>
       <Footer />
     </>
-  );
+  )
 }
