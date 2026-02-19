00ed73ac757033ba389e82d1f97d9827af966d47
Angel Rangel <angel145256@gmail.com>
2026-02-19 13:38:42 -0600

    refactor: mejorar la tipificación y la legibilidad en el componente SongListItem

--- src/components/song-list-item.tsx
+++ src/components/song-list-item.tsx
@@ -1,35 +1,39 @@
-import Link from "next/link";
-import type { Song } from "@/lib/types";
-import { ChevronRight } from "lucide-react";
+import Link from 'next/link'
+import Image from 'next/image'
+import type { Song } from '@/types'
+import { ChevronRight } from 'lucide-react'
+import { JSX } from 'react'
 
 /**
  * Componente que muestra un elemento de canción en formato de lista.
  * @param {{ song: Song }} props - Propiedades del componente, contiene los datos de la canción.
  * @returns {JSX.Element} El elemento de la lista de canciones.
  */
-export function SongListItem({ song }: { song: Song }) {
+export function SongListItem ({ song }: { song: Song }): JSX.Element {
   return (
     <Link
       href={`/songs/${song.slug}`}
-      className="block transition-colors duration-200 rounded-lg hover:bg-secondary/80 group"
+      className='block transition-colors duration-200 rounded-lg hover:bg-secondary/80 group'
       aria-label={`Ver canción ${song.title} de ${song.artist}`}
       title={`Canción ${song.title} de ${song.artist}`}
     >
-      <section className="flex items-center gap-4 p-2 ">
-          <img
-            src={`${song.coverArt}`}
-            alt={`Portada de la cancion ${song.title} del artista ${song.artist}`}
-            className="h-12 w-20"
-            title={`Portada de la cancion ${song.title} del artista ${song.artist}`}
-          />
+      <section className='flex items-center gap-4 p-2 '>
+        <Image
+          src={`${song.coverArt ?? ''}`}
+          alt={`Portada de la cancion ${song.title} del artista ${song.artist}`}
+          title={`Portada de la cancion ${song.title} del artista ${song.artist}`}
+          decoding='async'
+          width='80'
+          height='48'
+        />
 
-        <div className="grow">
-          <p className="font-semibold text-black dark:text-white">
-            {song.title} - <span className="text-sm text">{song.artist}</span>
+        <div className='grow'>
+          <p className='font-semibold text-black dark:text-white'>
+            {song.title} - <span className='text-sm text'>{song.artist}</span>
           </p>
         </div>
-        <ChevronRight className="h-5 w-5 text-muted-foreground transition-transform group-hover:translate-x-1" />
+        <ChevronRight className='h-5 w-5 text-muted-foreground transition-transform group-hover:translate-x-1' />
       </section>
     </Link>
-  );
+  )
 }
