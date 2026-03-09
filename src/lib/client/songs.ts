aca02cb4a64026aaa976581fd1ff584291d0ee62
Angel Rangel <angel145256@gmail.com>
2026-03-09 11:35:24 -0600

    fix: quitando services que ya están por separado en un archivo mejor

--- src/lib/client/songs.ts
+++ src/lib/client/songs.ts
@@ -1,38 +1,21 @@
 // IMPORTANTE: Este archivo está destinado a operaciones del lado del cliente y NO debe tener la directiva 'use server'.
 import { supabase } from '@/lib/supabase'
-import type { Song } from '@/types'
+import type { Song } from '@/types/app.types'
 
 // Tipo para agregar una nueva canción, sin los campos generados por la base de datos (id, contadores).
-export type NewSongData = Omit<Song, 'id' | 'visitCount' | 'likeCount'>
+export type NewSongData = Omit<Song, 'id' | 'key' | 'createdAt' | 'isPublished' | 'updatedAt'>
 
 /**
  * Agrega una nueva canción a la base de datos.
  * @param {NewSongData} songData - Los datos de la nueva canción.
  */
 export async function addSong (songData: NewSongData): Promise<void> {
-  const { error } = await supabase.from('songs').insert({
-    ...songData,
-    visitCount: 0, // Inicializa los contadores.
-    likeCount: 0
-  })
+  const { error } = await supabase
+    .from('songs_2')
+    .insert({ ...songData })
   if (error != null) throw error
 }
 
-/**
- * Actualiza los datos de una canción existente en la base de datos.
- * @param {string} id - El ID de la canción a actualizar.
- * @param {Partial<NewSongData>} songData - Los campos de la canción a actualizar.
- */
-export async function updateSong (
-  id: string,
-  songData: Partial<NewSongData>
-): Promise<void> {
-  const { error } = await supabase
-    .from('songs')
-    .update(songData)
-    .eq('id', id)
-  if (error != null) throw error
-}
 
 /**
  * Elimina una canción de la base de datos por su ID.
