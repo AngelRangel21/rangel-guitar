f63c5e4f1dd9ad4a2a7ba4114ca7fbb5f05380bc
Angel Rangel <angel145256@gmail.com>
2026-03-09 11:38:07 -0600

    fix: mejorando la lógica y agregando nuevos services

--- src/services/song.service.ts
+++ src/services/song.service.ts
@@ -1,11 +1,15 @@
 import { supabase } from '@/lib/supabase'
 import { Song } from '@/types'
+import { SongWithArtist } from '@/types/app.types'
+
+export type NewSongData = Omit<Song, 'id' | 'key' | 'createdAt' | 'isPublished' | 'updatedAt'>
 
 export async function getSongs (): Promise<Song[]> {
   const { data, error } = await supabase
     .from('songs')
     .select('id, slug, title, artist, "coverArt", chords, lyrics') // ✅ Solo campos necesarios
     .order('title', { ascending: true })
+    .returns<Song[]>()
 
   if (error != null) {
     console.error('[getSongs] Error de Supabase:', error)
@@ -20,17 +24,67 @@ export async function getSongs (): Promise<Song[]> {
   }
 
   // ✅ Filtrar canciones con datos incompletos (precaución extra)
-  return data.filter((song): song is Song => {
-    const isValid =
-      typeof song.id === 'string' &&
-      typeof song.slug === 'string' &&
-      typeof song.title === 'string' &&
-      typeof song.artist === 'string'
+  return data
+}
 
-    if (!isValid) {
-      console.warn('[getSongs] Canción con datos incompletos ignorada:', song)
-    }
+export async function getSongByArtist (): Promise<SongWithArtist[]> {
+  const { data, error } = await supabase
+    .from('songs_2')
+    .select(`
+      *,
+      artist:artists (
+        id,
+        name,
+        slug
+      )
+    `)
+    .order('title', { ascending: true })
+    .returns<SongWithArtist[]>()
 
-    return isValid
-  })
+  if (data == null) {
+    console.error('[getSongByArtist]', Error)
+  }
+
+  if (error != null) {
+    throw new Error('Error al obtener las canciones', error)
+  }
+
+  return data
+}
+
+export async function getSongBySlug (slug: string): Promise<SongWithArtist> {
+  const { data, error } = await supabase
+    .from('songs_2')
+    .select('*, artist:artists (*)')
+    .eq('slug', slug)
+    .limit(1)
+    .single()
+    .returns<SongWithArtist>()
+  
+  if (error != null) throw error
+
+  return data
+}
+
+export async function addSong (songData: NewSongData): Promise<void> {
+  const { error } = await supabase
+    .from('songs_2')
+    .insert({ ...songData })
+  if (error != null) throw error
+}
+
+export async function updateSong (id: string, songData: Partial<SongWithArtist>): Promise<void> {
+  const { error } = await supabase
+    .from('songs_2')
+    .update(songData)
+    .eq('id', id)
+  if (error != null) throw error
+}
+
+export async function deleteSong (id: string): Promise<void> {
+  const { error } = await supabase
+    .from('songs_2')
+    .delete()
+    .eq('id', id)
+  if (error != null) throw error
 }
