7075c4a62aebac17db9cd17e4a66843b1a755bfb
Angel Rangel <angel145256@gmail.com>
2026-03-08 18:51:47 -0600

    fix: agregando url de supabase para las imágenes

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,4 @@
 import type { NextConfig } from 'next'
-// import withPWAInit from "@ducanh2912/next-pwa";
-
-// Inicializa el plugin de Next.js para Progressive Web Apps (PWA).
-// const withPWA = withPWAInit({
-//   dest: "public", // Directorio donde se guardarán los archivos de la PWA.
-//   register: true, // Registra el service worker automáticamente.
-//   disable: process.env.NODE_ENV === "development", // Desactiva la PWA en desarrollo.
-//   workboxOptions: {
-//     skipWaiting: true, // Fuerza al service worker a activarse inmediatamente.
-//     clientsClaim: true,
-//   },
-// });
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -30,10 +18,15 @@ const nextConfig: NextConfig = {
         hostname: 'placehold.co',
         port: '',
         pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+        port: '',
+        pathname: '/**'
       }
     ]
   }
 }
 
-// export default withPWA(nextConfig);
 export default nextConfig
