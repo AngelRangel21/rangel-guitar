56ffbf3082b92e7cfd31568604c77543adc9f832
Angel Rangel <angel145256@gmail.com>
2026-03-15 06:42:07 -0600

    fix: Nuevo provider

---
+++ src/auth/provider/auth.provider.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+import { supabase } from "@/lib/supabase";
+import { JSX, useEffect } from "react";
+import { useAuthStore } from "../stores/auth.stores";
+
+export function AuthProvider ({ children }: { children: React.ReactNode }): JSX.Element {
+  const loadUserProfile = useAuthStore((s) => s.loadUserProfile)
+    const _setInitialized = useAuthStore((s) => s._setInitialized)
+    const _setLoading = useAuthStore((s) => s._setLoading)
+    const logout = useAuthStore((s) => s.logout)
+  
+    // Sincronizar supabaseUser con el store
+    useEffect(() => {
+      const {
+        data: { subscription },
+      } = supabase.auth.onAuthStateChange(async (event, session) => {
+        console.debug('[Auth]', event, session?.user?.id)
+  
+        switch (event) {
+          case 'INITIAL_SESSION':
+            if (session?.user != null) {
+              await loadUserProfile(session.user)
+            }
+            _setInitialized(true)
+            _setLoading(false)
+            break
+            
+          case 'SIGNED_IN':
+            if (session?.user != null) {
+              await loadUserProfile(session.user)
+            }
+            break
+          
+          case 'SIGNED_OUT':
+            useAuthStore.setState({
+              supabaseUser: null,
+              user: null,
+              favoriteIds: new Set()
+            })
+          
+          case 'USER_UPDATED':
+            if (session?.user != null) {
+              await loadUserProfile(session.user)
+            }
+            break
+  
+            case 'TOKEN_REFRESHED':
+              break
+        }
+      })
+  
+      return () => {
+        subscription.unsubscribe()
+      }
+    }, [loadUserProfile, _setInitialized, _setLoading, logout])
+
+  return <>{children}</>
+}
