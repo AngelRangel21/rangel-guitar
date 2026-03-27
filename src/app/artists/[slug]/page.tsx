10d9690716214886ce811bc27d74bb5de97c237f
Angel Rangel <angel145256@gmail.com>
2026-03-27 08:12:09 -0600

    fix:  Eliminando el name del page para usar el slug

--- src/app/artists/[slug]/page.tsx
+++ src/app/artists/[slug]/page.tsx
@@ -1,33 +1,21 @@
 import type { Metadata } from 'next'
-import { notFound } from 'next/navigation'
 import { ArtistDetailContent } from '@/components/artist-detail-content'
 import { JSX } from 'react'
 import { getArtistImage, getSongsByArtistSlug } from '@/services/artists.service'
 
-export const dynamic = 'force-dynamic'
-export const revalidate = 3600
-
-// No necesitamos generateStaticParams ya que usaremos generación dinámica
-
-/**
- * Genera los metadatos (título, descripción, etc.) para la página de un artista específico.
- * Es crucial para el SEO y para cómo se muestra la página al compartirla en redes sociales.
- * @param {{ params: { name: string } }} props - Las propiedades de la página, incluyendo el nombre del artista desde la URL.
- * @returns {Promise<Metadata>} El objeto de metadatos para la página.
- */
 export async function generateMetadata ({
   params
 }: {
-  params: Promise<{ name: string }>
+  params: Promise<{ slug: string }>
 }): Promise<Metadata> {
-  const { name } = await params
-  const songs = await getSongsByArtistSlug(name)
+  const { slug } = await params
+  const songs = await getSongsByArtistSlug(slug)
   const artistName = songs[0].artist.name
-  
+
   const description = `Explora todas las canciones y tablaturas de ${artistName} en Rangel Guitar. Aprende a tocar sus éxitos en guitarra.`
   const title = `Canciones de ${artistName}`
-  
-  const image = `${decodeURIComponent(name)}.webp`
+
+  const image = `${decodeURIComponent(slug)}.webp`
   const imageUrl = image ? getArtistImage(image) : 'https://placehold.co/1200x630.png'
 
   return {
@@ -58,15 +46,11 @@ export async function generateMetadata ({
 export default async function ArtistDetailPage ({
   params
 }: {
-  params: Promise<{ name: string }>
+  params: Promise<{ slug: string }>
 }): Promise<JSX.Element> {
-  const { name } = await params
-  const songs = await getSongsByArtistSlug(name)
+  const { slug } = await params
+  const songs = await getSongsByArtistSlug(slug)
   const artistName = songs[0].artist.name
-
-  if (!songs || songs.length === 0) {
-    notFound()
-  }
 
   return (
     <div className='flex flex-col min-h-screen bg-background'>
